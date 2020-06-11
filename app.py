009561aa5e432a0ddbec2f5356e481202d8821f6
Daniel Levine <dflevine13@gmail.com>
2020-06-11 13:30:07 -0400

    Second commit

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect
+from bokeh.plotting import figure
+from bokeh.embed import components
 
 app = Flask(__name__)
 
@@ -12,3 +14,21 @@ def about():
 
 if __name__ == '__main__':
   app.run(port=33507)
+
+@app.route('/dashboard/')
+def show_dashboard():
+    plots = []
+    plots.append(make_plot())
+
+    return render_template('dashboard.html', plots=plots)
+
+def make_plot():
+    plot = figure(plot_height=300, sizing_mode='scale_width')
+
+    x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    y = [2**v for v in x]
+
+    plot.line(x, y, line_width=4)
+
+    script, div = components(plot)
+    return script, div
