af1ea25ba9c47bf449df4a90d5132c6dbf595c4c
Daniel Levine <dflevine13@gmail.com>
2020-06-14 14:03:50 -0400

    forms3

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 import selection
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    script, div = selection.plot('NVDA', 'close')
+@app.route('/plot/<ticker>/<column>')
+def plot(ticker, column):
+    script, div = selection.plot(ticker, column)
 
     return render_template('index.html', div=div, script=script)
+
+
+@app.route('/', methods=['POST'])
+def index():
+    ticker = request.form(['ticker'])
+    column = request.form(['column'])
+
+    return redirect(url_for('plot', ticker=ticker, column=column))
 
 
 if __name__ == '__main__':
