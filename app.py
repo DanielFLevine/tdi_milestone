e9b79a04c696f69fef03785d6d6d699b822a245a
Daniel Levine <dflevine13@gmail.com>
2020-06-13 21:37:18 -0400

    plot ts7

--- app.py
+++ app.py
@@ -11,15 +11,15 @@ app = Flask(__name__)
 @app.route('/')
 def bokeh_plot():
     data_nvda = requests.get(
-        r'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=NVDA&interval=1min&outputsize=full&apikey=9QO32QU9D5ADYS17')
+        r'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=NVDA&interval=60min&outputsize=full&apikey=9QO32QU9D5ADYS17')
     data_amd = requests.get(
-        r'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AMD&interval=1min&outputsize=full&apikey=9QO32QU9D5ADYS17')
+        r'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AMD&interval=60min&outputsize=full&apikey=9QO32QU9D5ADYS17')
 
     data_nvda = data_nvda.json()
     data_amd = data_amd.json()
 
-    df_nvda = pd.DataFrame.from_dict(data_nvda["Time Series (1min)"], orient='index')
-    df_amd = pd.DataFrame.from_dict(data_amd["Time Series (1min)"], orient='index')
+    df_nvda = pd.DataFrame.from_dict(data_nvda["Time Series (60min)"], orient='index')
+    df_amd = pd.DataFrame.from_dict(data_amd["Time Series (60min)"], orient='index')
 
     df_nvda['datetime'] = df_nvda.index.astype('datetime64[ns]')
     df_amd['datetime'] = df_amd.index.astype('datetime64[ns]')
@@ -36,7 +36,7 @@ def bokeh_plot():
                 i[j] = i[j].astype('float')
         i['return percent'] = (i['close'] - i['open']) / i['open']
 
-    p = figure(plot_height=400, plot_width=12000)
+    p = figure(plot_height=400, plot_width=1000)
     r_nvda = p.line(x=df_amd.index, y=df_nvda['return percent'])
     r_amd = p.line(x=df_amd.index, y=df_amd['return percent'], color='green')
 
