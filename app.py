382580850da260784a29601459faacb2259da722
Daniel Levine <dflevine13@gmail.com>
2020-06-17 14:58:28 -0400

    design

--- app.py
+++ app.py
@@ -5,13 +5,6 @@ import selection
 app = Flask(__name__)
 
 
-#@app.route('/<ticker>/<column>', methods=["GET"])
-#def plot(ticker, column):
-#    script, div = selection.plot(ticker, column)
-
-#    return render_template('index.html', div=div, script=script)
-
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
